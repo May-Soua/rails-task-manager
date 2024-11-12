64cf16629f2aec3f34cf4118b180489dfd96bddb
May Ya <maysoua.y@gmail.com>
2024-11-12 22:49:45 +0100

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # lister les taches
+  get "tasks", to: "tasks#index"
+  # ajouter une nouvelle tache
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # afficher les détails
+  get "tasks/:id", to: "tasks#show", as: :task
+  # éditer (marqué comme terminé/mettre à jour)
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  #  supprime une tache
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
+
+
 end
